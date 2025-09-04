abd733c6ce1f1eb6233bea1836204d60c9f69a9b
Nanda Irjayandi <nanda.irjayandi@gmail.com>
2025-09-04 23:09:37 +1000

    fix: issue with timing

--- boards/shields/nice_view_gem/widgets/animation.c
+++ boards/shields/nice_view_gem/widgets/animation.c
@@ -25,7 +25,7 @@ void draw_animation(lv_obj_t *canvas) {
     lv_obj_t *art = lv_animimg_create(canvas);
     lv_obj_center(art);
 
-    lv_animimg_set_src(art, (const void **)anim_imgs, 13);
+    lv_animimg_set_src(art, (const void **)anim_imgs, 11);
     lv_animimg_set_duration(art, CONFIG_NICE_VIEW_GEM_ANIMATION_MS);
     lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);
     lv_animimg_start(art);
